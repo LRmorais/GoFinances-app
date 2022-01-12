3587fca8cae1c0b44d585e88f8aa50066bd70636
Lucas Morais <lucas.morais23ti@gmail.com>
2022-01-12 10:22:57 -0300

    features

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,30 +1,65 @@
 import React from 'react';
-import { Text } from 'react-native';
-import AppLoading from 'expo-app-loading'
+import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard } from '../../components/TransactionCard';
 
-import {
-useFonts,
-Poppins_400Regular,
-Poppins_500Medium,
-Poppins_700Bold
-} from '@expo-google-fonts/poppins';
-
-import { Container } from './styles';
+import { 
+  Container,
+  Header,
+  UserWeapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGretting,
+  UserName,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+} from './styles';
 
 export function Dashboard(){
-  const [fontsLoaded] = useFonts({
-    Poppins_400Regular,
-    Poppins_500Medium,
-    Poppins_700Bold
-  });
-
-  if(!fontsLoaded){
-    return <AppLoading />
-  }
 
   return(
     <Container>
-      <Text>Dashboard</Text>
+      <Header>
+        <UserWeapper>
+          <UserInfo>
+            <Photo source={{uri: 'https://avatars.githubusercontent.com/u/60013557?v=4'}} />
+            <User>
+              <UserGretting>Olá, </UserGretting>
+              <UserName>Lucas </UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWeapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard 
+        type='up'
+        title='Entrada'
+        amount='R$ 17.400,00' 
+        lastTransaction='Última entrada dia 13 de abril' />
+        <HighlightCard 
+        type='down'
+        title='Saídas' 
+        amount='R$ 1.259,00' 
+        lastTransaction='Última saída dia 03 de abril' />
+        <HighlightCard 
+        type='total'
+        title='Total' 
+        amount='R$ 16.141,00' 
+        lastTransaction='01 à 16 de abril' />
+
+      </HighlightCards>
+
+      <Transactions>
+        <Title>
+          Listagem
+        </Title>
+
+        <TransactionCard />
+      </Transactions>
     </Container>
   )
 }
